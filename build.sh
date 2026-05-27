db30e7c30417669d26dfe9742bb0731c3b8aee31
Juhana Lankinen <juhana.lankinen@csc.fi>
2026-05-27 14:57:28 +0300

    Add build script; Change run suitable for profiling

---
+++ build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -l
+
+#SBATCH --account=project_462001358
+#SBATCH --partition=standard-g
+#SBATCH --job-name=build-runko
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-task=56
+#SBATCH --mem-per-cpu=8GB
+#SBATCH --time=00:20:00
+
+module load LUMI/25.09
+module load partition/G
+module load PrgEnv-cray
+module load rocm/6.4.4
+module load craype-accel-amd-gfx90a
+module load cray-mpich/9.0.1
+module load craype-network-ofi
+module load buildtools
+module load cray-python
+module load lumi-CrayPath
+
+if [ ! -d ${RUNKODIR} ]
+then
+    echo "Set the variable RUNKODIR to point to the runko repository directory"
+    exit 1
+fi
+
+cd ${RUNKODIR}
+
+python -m venv venv
+
+source venv/bin/activate
+
+MPI4PY_BUILD_MPICC=cc python -m pip install --no-cache-dir --no-binary=mpi4py mpi4py
+
+python -m pip install \
+    pybind11 \
+    scikit-build-core \
+    viztracer
+
+pip install --no-build-isolation -v -e /projappl/project_462001358/juhanala/runko \
+      --config-settings=cmake.args=--preset=lumi-gpu
+
+# Make squashfs?
--- projects/pic-turbulence/pic.py
+++ projects/pic-turbulence/pic.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     config.NyMesh = 64
     config.NzMesh = 64
     config.cfl = 0.45
-    config.Nt = 5.0*config.Nx*config.NxMesh/config.cfl/3.0 # =5eddy turnover times
+    config.Nt = 10
     config.xmin = 0
     config.ymin = 0
     config.zmin = 0
@@ -44,10 +44,9 @@ if __name__ == "__main__":
     config.tile_partitioning = "hilbert_curve"
     config.laps_in_timer_statistics = 20
 
-
     # Problem specific configuration
-    ppc = 2 # particles per cell (one particle type)
-    oppc = 2 * ppc # overall particles per cell (all particle types)
+    ppc = 2  # particles per cell (one particle type)
+    oppc = 2 * ppc  # overall particles per cell (all particle types)
     gamma = 1
     c_omp = 1
     omp = config.cfl / c_omp
@@ -58,9 +57,9 @@ if __name__ == "__main__":
     m0 = config.m0 * abs(config.q0)
     m1 = config.m1 * abs(config.q1)
 
-    delgam = 0.3 # temperature
-    temp_ration = 1 # T_i / T_e
-    sigma = 10 # magnetization (omega_ce/omega_pe)^2
+    delgam = 0.3  # temperature
+    temp_ration = 1  # T_i / T_e
+    sigma = 10  # magnetization (omega_ce/omega_pe)^2
 
     delgam0 = delgam
     delgam1 = temp_ration * delgam0
@@ -68,15 +67,15 @@ if __name__ == "__main__":
     # No corrections; cold sigma
     binit_nc = np.sqrt(oppc * (config.cfl**2.0) * sigma * m0)
     # another approximation which is more accurate at \delta ~ 1
-    gammath = 1.0 + (3.0/2.0) * delgam1
+    gammath = 1.0 + (3.0 / 2.0) * delgam1
 
     binit_approx = np.sqrt(gammath * oppc * m0 * (config.cfl**2.0) * sigma)
-    binit = binit_approx # NOTE: selecting this as our sigma definitions
+    binit = binit_approx  # NOTE: selecting this as our sigma definitions
 
     logger.info(f"Positron thermal spread: {delgam1}")
     logger.info(f"Electron thermal spread: {delgam0}")
     logger.info(f"Alfven vel: {np.sqrt(sigma / (1. + sigma))}")
-    ion_beta = 2. * delgam1 / (sigma * (m1 /m0 + 1.) / (m1 /m0 ))
+    ion_beta = 2.0 * delgam1 / (sigma * (m1 / m0 + 1.0) / (m1 / m0))
     logger.info(f"Ion beta: {ion_beta}")
     logger.info(f"Electron beta: {2.*delgam0/(sigma*(1/m0+1.))}")
     logger.info(f"sigma: {sigma}")
@@ -116,10 +115,10 @@ if __name__ == "__main__":
 
     def Bx(x, y, z):
         bx = np.zeros_like(x)
-        x, y = y, x # done indirectly in pic-trubulence/antenna3d.py
+        x, y = y, x  # done indirectly in pic-trubulence/antenna3d.py
         I = range(1, n_perp + 1)
         for n, m in itertools.product(I, I):
-            norm = beta(n ,m)
+            norm = beta(n, m)
             for o in range(1, n_par + 1):
                 xmodx = np.sin(m * kx * x + ph1[n - 1, m - 1, o - 1])
                 xmody = np.cos(n * ky * y + ph2[n - 1, m - 1, o - 1])
@@ -130,10 +129,10 @@ if __name__ == "__main__":
 
     def By(x, y, z):
         by = np.zeros_like(x)
-        x, y = y, x # done indirectly in pic-trubulence/antenna3d.py
+        x, y = y, x  # done indirectly in pic-trubulence/antenna3d.py
         I = range(1, n_perp + 1)
         for n, m in itertools.product(I, I):
-            norm = beta(n ,m)
+            norm = beta(n, m)
             for o in range(1, n_par + 1):
                 ymodx = np.cos(m * kx * x + ph1[n - 1, m - 1, o - 1])
                 ymody = np.sin(n * ky * y + ph2[n - 1, m - 1, o - 1])
@@ -144,7 +143,6 @@ if __name__ == "__main__":
 
     Bz = lambda x, y, z: np.full_like(x, binit)
     zero_field = lambda x, y, z: np.zeros_like(x)
-
 
     def pgen0(x, y, z):
         N = len(x)
@@ -172,9 +170,17 @@ if __name__ == "__main__":
     if not tile_grid.initialized_from_restart_file():
         for idx in tile_grid.local_tile_indices():
             tile = runko.pic.threeD.Tile(idx, config)
-            tile.batch_set_EBJ(zero_field, zero_field, zero_field,
-                               Bx, By, Bz,
-                               zero_field, zero_field, zero_field)
+            tile.batch_set_EBJ(
+                zero_field,
+                zero_field,
+                zero_field,
+                Bx,
+                By,
+                Bz,
+                zero_field,
+                zero_field,
+                zero_field,
+            )
             for _ in range(ppc):
                 runko.runko_logger().info("Injecting particles of type 0...")
                 tile.batch_inject_to_cells(0, pgen0)
