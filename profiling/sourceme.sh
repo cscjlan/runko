463550c4e73f60836226f13164a07a5642e499d6
Juhana Lankinen <juhana.lankinen@csc.fi>
2026-07-01 10:11:49 +0300

    Add a sourced file that loads modules and activates venv

---
+++ profiling/sourceme.sh
@@ -0,0 +1,11 @@
+module load LUMI/25.09
+module load partition/G
+module load PrgEnv-cray
+module load rocm/6.4.4
+module load craype-accel-amd-gfx90a
+module load cray-mpich/9.0.1
+module load craype-network-ofi
+module load buildtools
+module load lumi-CrayPath
+
+source venv/bin/activate
